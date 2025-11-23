945c1fd7d4f0d9d4d75e811768b6ae8f0d209812
TheRyanLee <rlee@carrollu.edu>
2025-11-23 14:16:07 -0600

    More new stuff

--- app/src/main/java/com/example/restaurantadvisor/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/restaurantadvisor/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.restaurantadvisor.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,9 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.restaurantadvisor.R;
 import com.example.restaurantadvisor.databinding.FragmentHomeBinding;
+import com.example.restaurantadvisor.restaurantinfo;
 
 public class HomeFragment extends Fragment {
 
@@ -26,6 +29,20 @@ public class HomeFragment extends Fragment {
 
         //final TextView textView = binding.textHome;
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.culvers.setOnClickListener(this::selectedRestaurant);
+        binding.arbys.setOnClickListener(this::selectedRestaurant);
+        binding.dairyqueen.setOnClickListener(this::selectedRestaurant);
+        binding.wendys.setOnClickListener(this::selectedRestaurant);
+        binding.tacobell.setOnClickListener(this::selectedRestaurant);
+        binding.subway.setOnClickListener(this::selectedRestaurant);
+        binding.pizzaranch.setOnClickListener(this::selectedRestaurant);
+        binding.papajohns.setOnClickListener(this::selectedRestaurant);
+        binding.mcdonalds.setOnClickListener(this::selectedRestaurant);
+        binding.littlecaesars.setOnClickListener(this::selectedRestaurant);
+        binding.kfc.setOnClickListener(this::selectedRestaurant);
+
+
         return root;
     }
 
@@ -34,4 +51,45 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void selectedRestaurant(View view) {
+        int id = view.getId();
+
+        String restaurantName = "";
+
+
+        if (id == binding.culvers.getId()) {
+            restaurantName = "culvers";
+        } else if (id == binding.arbys.getId()) {
+            restaurantName = "arbys";
+        } else if (id == binding.dairyqueen.getId()) {
+            restaurantName = "dairyqueen";
+        } else if (id == binding.wendys.getId()) {
+            restaurantName = "wendys";
+        } else if (id == binding.tacobell.getId()) {
+            restaurantName = "tacobell";
+        } else if (id == binding.subway.getId()) {
+            restaurantName = "subway";
+        } else if (id == binding.pizzaranch.getId()) {
+            restaurantName = "pizzaranch";
+        } else if (id == binding.papajohns.getId()) {
+            restaurantName = "papajohns";
+        } else if (id == binding.mcdonalds.getId()) {
+            restaurantName = "mcdonalds";
+        } else if (id == binding.littlecaesars.getId()) {
+            restaurantName = "littlecaesars";
+        } else if (id == binding.kfc.getId()) {
+            restaurantName = "kfc";
+        }
+
+        if(!restaurantName.isEmpty()){
+            Intent newAct = new Intent(getActivity(), restaurantinfo.class);
+            newAct.putExtra("restaurantname", restaurantName);
+            startActivity(newAct);
+        }
+
+    }
+
+
+
 }
