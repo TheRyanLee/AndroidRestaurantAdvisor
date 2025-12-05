90c34e3e8989bda9bc0d180597f4fc798fb90651
TheRyanLee <rlee@carrollu.edu>
2025-12-04 19:12:18 -0600

    Added restaurantinfo to display when certain restaurant selected.

--- app/src/main/java/com/example/restaurantadvisor/restaurantinfo.java
+++ app/src/main/java/com/example/restaurantadvisor/restaurantinfo.java
@@ -1,7 +1,10 @@
 package com.example.restaurantadvisor;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +15,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,7 +34,7 @@ public class restaurantinfo extends AppCompatActivity {
         String restaurantName = getIntent().getStringExtra("restaurantname");
         String cRestaurantName = restaurantName.substring(0,1).toUpperCase() + restaurantName.substring(1);
 
-        TextView tv = findViewById(R.id.dispalyRestaurantName);
+        TextView tv = (TextView) findViewById(R.id.dispalyRestaurantName);
 
         tv.setText(cRestaurantName);
 
@@ -49,7 +57,30 @@ public class restaurantinfo extends AppCompatActivity {
             restaurantImage.setImageResource(restaurantImages.get(restaurantName));
         }
 
+        TextView tv2 = (TextView) findViewById(R.id.displayInfo);
 
+
+        copyDatabaseIfNeeded();
+        String dbPath = getDatabasePath("restaurantinfo.sqlite").getPath();
+        SQLiteDatabase myDB = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READONLY);
+        String tablename = "restaurants";
+
+
+        String query = "SELECT address, hours, type_of_dining, type_of_cuisine FROM " + tablename + " WHERE restaurant_name LIKE '" + restaurantName + "';";
+        Cursor crs = myDB.rawQuery(query, null);
+
+
+        String rInfo = "";
+
+        if(crs.moveToFirst())
+        {
+            rInfo = crs.getString(0) + "\n" + crs.getString(1) + "\n" + crs.getString(2) + "\n" + crs.getString(3);
+        }
+
+        tv2.setText(rInfo);
+
+        crs.close();
+        myDB.close();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -57,6 +88,37 @@ public class restaurantinfo extends AppCompatActivity {
             return insets;
         });
     }
+
+    private void copyDatabaseIfNeeded() {
+        String dbName = "restaurantinfo.sqlite";
+        File dbFile = getDatabasePath(dbName);
+
+        if (dbFile.exists()) {
+            return;
+        }
+
+        dbFile.getParentFile().mkdirs();
+
+        try {
+            InputStream is = getAssets().open("databases/" + dbName);
+            OutputStream os = new FileOutputStream(dbFile);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+
+            os.flush();
+            os.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error copying preloaded database");
+        }
+    }
+
+
 
     public void goBack (View v)
     {
